6818cc4df35ca7c9e3d2fc321296b633e308420f
pbmariano <pablo.almeida.mariano@gmail.com>
2022-01-30 15:14:47 -0300

    Initial commit

---
+++ src/dios/java/com/Exercicio16.java
@@ -0,0 +1,14 @@
+package dios.java.com;
+
+public class Exercicio16 {
+    public static void main(String[] agrs){
+    double num1=0, num2=1, num3, i=1;
+    while(i <500 ){
+        i++;
+        num3 = num1 + num2;
+        num1 = num2;
+        num2 = num3;
+        System.out.println(num3);
+    }
+    }
+}
